8632a166f32c5bfbcd84f34cf5f8c2e4656877e7
Marcus Davidsson <marcus.davidsson@iths.se>
2022-02-15 10:38:20 +0100

    GREEN

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -2,14 +2,19 @@ public class Employee {
 
     private int ID;
     private static int counter;
-
+    private String firstName;
 
     public Employee(String firstName, String lastName, int age, double salary){
         counter = counter + 1;
         ID = counter;
+        this.firstName = firstName;
     }
 
     public int getUniqueID() {
         return ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
     }
 }
