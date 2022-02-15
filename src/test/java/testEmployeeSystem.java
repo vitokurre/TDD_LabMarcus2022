a98f2ce4da0a099a0f19a1cdc89d1eecc724ba8e
Marcus Davidsson <marcus.davidsson@iths.se>
2022-02-15 13:15:02 +0100

    RED

---
+++ src/test/java/testEmployeeSystem.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.*;
+
+public class testEmployeeSystem {
+        Employee testObject1;
+        EmployeeSystem testObjectSystem;
+
+
+        @BeforeAll
+        public static void beforeAll(){
+            System.out.println("Nu börjar testsviten för Labb 1 i TDD");
+        }
+        @AfterAll
+        public static void afterAll(){
+            System.out.println("Nu avslutas testsviten");
+        }
+        @AfterEach
+        public void afterEach(){
+            System.out.println("Test slut");
+        }
+        @BeforeEach
+        public void beforeEach() {
+            System.out.println("Test start");
+
+            testObject1 = new Employee("Marcus", "Davidsson", 33, 30500.00);
+
+        }
+
+        @Test
+        public void checkGetEmployeeListTest(){
+
+            int expected = 3;
+
+
+
+
+        }
+    }
